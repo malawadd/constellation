d4711b9206f6db9d6eafbc55347996191fc0dbdf
malawad <50057882+malawadd@users.noreply.github.com>
2023-12-10 23:18:43 +0300

    ❤️ nextjs ❤️

---
+++ webapp/pages/_app.tsx
@@ -0,0 +1,56 @@
+import { WagmiConfig, configureChains, createConfig } from "wagmi"
+import Header from "../components/Header"
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { goerli } from "viem/chains"
+import { createWeb3Modal, walletConnectProvider } from "@web3modal/wagmi"
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect"
+import { InjectedConnector } from "wagmi/connectors/injected"
+import { MetaMaskConnector } from "wagmi/connectors/metaMask"
+import { ThemeProvider } from "styled-components"
+import { ThorinGlobalStyles, lightTheme, darkTheme } from "@ensdomains/thorin"
+import Head from "next/head"
+
+// 1. Get projectId
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string
+
+// 2. Create wagmiConfig
+const { chains, publicClient } = configureChains(
+  [goerli],
+  [walletConnectProvider({ projectId })]
+)
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: [
+    new WalletConnectConnector({ options: { projectId, showQrModal: false } }),
+    new InjectedConnector({ options: { shimDisconnect: true } }),
+    new MetaMaskConnector({ chains }),
+  ],
+  publicClient,
+})
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains })
+
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <Head>
+        <title>Hasala</title>
+        <meta name="description" content="Hasala" key="desc" />
+      </Head>
+      <ThemeProvider theme={lightTheme}>
+        <ThorinGlobalStyles />
+        <div className="container mx-auto px-60">
+          <Header />
+          <main className="">
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </ThemeProvider>
+    </WagmiConfig>
+  )
+}
