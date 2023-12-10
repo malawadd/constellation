b3f890d51d201d72e15c9f955554a3827b897405
malawad <50057882+malawadd@users.noreply.github.com>
2023-12-10 23:20:15 +0300

    🦊 header 🦊

---
+++ webapp/components/Header/index.tsx
@@ -0,0 +1,60 @@
+import { OutlinkSVG, RecordItem } from "@ensdomains/thorin"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { fetchEnsName } from "@wagmi/core"
+import { useAccount } from "wagmi"
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "w3m-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >
+    }
+  }
+}
+
+export default function Header() {
+  const { address } = useAccount()
+  const [ensName, setEnsName] = useState<string>()
+
+  useEffect(() => {
+    getEnsName().then((ens: string | null) => {
+      if (ens) {
+        setEnsName(ens)
+      }
+    })
+  }, [address])
+
+  const getEnsName = async () => {
+    if (address) {
+      return await fetchEnsName({
+        address: address as `0x${string}`,
+      })
+    } else {
+      return ""
+    }
+  }
+
+  return (
+    <div className="py-8 flex justify-between">
+      <div className="font-mono text-3xl">
+        <Link href={"/"}>Hasala</Link>
+      </div>
+      <div className="flex gap-4 items-center">
+        <Link href={"/create"}>Create Plan</Link>
+        <Link href={"/transactions"}>Transactions</Link>
+        <Link
+          className="flex gap-2"
+          href={`https://app.ens.domains/${ensName || ""}?tab=subnames`}
+          target="_blank"
+        >
+          Set ENS <OutlinkSVG />
+        </Link>
+
+        <w3m-button />
+      </div>
+    </div>
+  )
+}
