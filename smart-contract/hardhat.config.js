13f16ee6c80ab643533ba5276ca1beca69f0c05f
malawad <50057882+malawadd@users.noreply.github.com>
2023-12-10 18:53:19 +0300

    ✅ swap contract ✅

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,34 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+   
+    },
+    goerli: {
+    chainId: 5,
+    url: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    accounts: [process.env.PRIVATE_KEY],
+
+  }
+
+  },
+ 
+  solidity: {
+    compilers: [{
+      version: "0.8.9",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }]
+  }
 };
--- smart-contract/scripts/swap.js
+++ smart-contract/scripts/swap.js
@@ -7,18 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const Swap = await hre.ethers.getContractFactory("SwapProxy");
+  const swap = await Swap.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await swap.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("swap deployed to ", swap.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -27,3 +21,6 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// npx hardhat run scripts/swap.js
